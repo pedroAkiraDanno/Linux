e94bd2b0c9a85d39bd2d911c36d613efc096493a
pedro akira <pedro-akira@hotmail.com>
2020-01-02 02:08:56 -0300

    add new folder and file about course LPI

--- Preparatorio_para_Certificacao_Linux_LPIC-1_Atualizado_V5/script.sh
+++ Preparatorio_para_Certificacao_Linux_LPIC-1_Atualizado_V5/script.sh
@@ -1,67 +1,36 @@
-cd Comandos_GNU_e_Unix-topico_103
+cd O_Legado_LPIC-1_4.0_Assuntos_Removidos_da_Versao_4.0_para_a_5.0
 
-touch Topico_103-Introducao_da_secao.txt
 
-touch 103.1_Trabalhando_na_Linha_de_Comando-shell_bash_echo_type_PATH.txt
+touch Topico_102-Assuntos_Removidos.txt
 
-touch 103.1_Trabalhando_na_Linha_de_Comando-Variaveis_de_Ambiente.txt
+touch Topico_104-104.4_Removido-Gerenciando_Cotas_de_Disco-quota.txt
 
-touch 103.1_Trabalhando_na_Linha_de_Comando-Comandos_Sequenciais_history_man.txt
+touch Topico_105-105.3_Removido-Gerenciamento_de_Dados_SQL-SELECT.txt
 
-touch 103.1_Trabalhando_na_Linha_de_Comando-uname_alias.txt
+touch Topico_105-105.3_Removido-Gerenciamento_de_Dados_SQL-SELECT.txt
 
-touch 103.1_Trabalhando_a_Linha_de_Comando-which.txt
+touch Topico_105-105.3_Removido-Gerenciamento_de_Dados_SQL-INSERT_UPDATE_DELETE.txt
 
-touch 103.1_Trabalhando_na_Linha_de_Comando-Quoting.txt
+touch Topico_105-Assuntos_Removidos_105.1-lists.txt
 
-touch 103.2_Aplicando_Filtros_a_Textos_e_Arquivos-cat_head_tail_sort_less_wc.txt
+touch Topico_106-Assuntos_Removidos_106.1-Fontes_no_X.txt
 
-touch 103.2_Aplicando_Filtros_a_Textos_e_Arquivos-uniq_od_paste_split.txt
+touch Topico_106-Assuntos_Removidos_106.2-Display_Manager_lightdm.txt
 
-touch 103.2_Aplicando_Filtros_a_Textos_e_Arquivos-tr_cut_sed.txt
+touch Topico_107-Assuntos_Removidos_107.2-anacron.txt 
 
-touch 103.2_Aplicando_Filtros_a_Textos_e_Arquivos-xzcat_bzcat_zcat.txt
+touch Topico_108-Assuntos_Removidos_108.1-syslog.txt
 
-touch 103.2_Aplicando_Filtros_a_Textos_e_Arquivos-checksums.txt
+touch Topico_108-Assuntos_Removidos_108.1-syslog_e_klog.txt
 
-touch 103.3_Gerencimanento_Basico_de_Arquivos-cd_ls_file.txt
+touch Topico_108-Assuntos_Removidos_108.3-Qmail.txt
 
-touch 103.3_Gerencimanento_Basico_de_Arquivos_touch_cp_mv.txt
+touch Topico_109-Alteracoes_109.2-etc_hostname_hosts_networks_nsswitch.txt
 
-touch 103.3_Gerencimanento_Basico_de_Arquivos_rm_mkdir_rmdir_find.txt
+touch Topico_109-Alteracoes_109.2-Comandos.txt
 
-touch 103.3_Gerencimanento_Basico_de_Arquivos-tar_gzip_bzip2_xz.txt
+touch Topico_109-Alteracoes_109.2-Arquivos_de_Configuracao.txt
 
-touch 103.3_Gerencimanento_Basico_de_Arquivos-cpio,_dd.txt
+touch Topico_109-Alteracoes_109.3-ping_traceroute_host_nslookup_dig.txt
 
-touch 103.4_Fluxos_Pipes_e_Redirecionamentos-Redir_entrada_saida_erro_pipe.txt 
-
-touch 103.4_Fluxos_Pipes_e_Redirecionamentos-tee_xargs_subcomandos.txt
-
-touch 103.4_Fluxos_Pipes_e_Redirecionamentos-Outros_Redirecionadores.txt
-
-touch 103.5_Criar_Monitorar_e_Encerrar_Processos-processos_ps_pstree_pgrep.txt
-
-touch 103.5_Criar_Monitorar_e_Encerrar_Processos-top.txt
-
-touch 103.5_Criar_Monitorar_e_Encerrar_Processos-kill_killall_pkill.txt
-
-touch 103.5_Criar_Monitorar_e_Encerrar_Processos-uptime_free_screen.txt
-
-touch 103.5_Criar_Monitorar_e_Encerrar_Processos-jobs_bg_fg_nohup.txt
-
-touch 103.5_Criar_Monitorar_e_Encerrar_Processos-watch_tmux.txt
-
-touch 103.6_Modificar_a_Prioridade_de_Execução_de_Processos-nice_renice.txt
-
-touch 103.7_Pesquisar_Arquivos_de_Texto_com_Expressões_Regulares-grep_intro_regex.txt
-
-touch 103.7_Pesquisar_Arquivos_de_Texto_com_Expressoes_Regulares-regex_sed.txt
-
-touch 103.7_Pesquisar_Arquivos_de_Texto_com_Expressao_Regular.txt
-
-touch 103.8_Edicao_Basica_de_Arquivos-vi-vim.txt
-
-touch 103.8_Edicao_Basica_de_Arquivos-nano_e_emacs.txt
-
-touch 103.8_Edicao_Basica_de_Arquivos-Definir_Editor_Padrao.txt
+touch Topico_109-Alteracoes_109.3-netstat_telnet_netcat_tcpdump.txt

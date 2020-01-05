e6084811c263ac17ce8d1203d10d592fc3237d1e
pedro akira <pedro-akira@hotmail.com>
2020-01-05 12:28:05 -0300

    add new folders and files about course shell_script

---
+++ Conceitos_de_Programacao_em_Shell_Script_Curso_Gratuito/script.sh
@@ -0,0 +1,26 @@
+mkdir conceitos_de_programacao_em_shell_script
+
+cd conceitos_de_programacao_em_shell_script
+
+touch introducao.txt
+
+touch como_intalar_linux_em_um_pc_ou_mac.txt
+
+touch elememntos_principais_de_um_programa_shell.txt
+
+touch variaveis.txt
+
+touch comentarios_e_leitura_de_codigo.txt
+
+touch subshell_e_substituicao_de_comandos.txt
+
+touch argumentos_posicionais.txt
+
+touch criacao_de_programas_complexos.txt
+
+touch condicionais_usando_if_then_else.txt
+
+touch o_for_loop.txt
+
+touch o_while_loop.txt
+

bb96d23d19b65754defc939020c12af447764343
pedroAkiraDanno <pedro-akira@hotmail.com>
2020-03-18 14:07:08 -0300

    add new lines about command ls,file and cd

--- Scripts/gitscript.sh
+++ Scripts/gitscript.sh
@@ -6,10 +6,16 @@
 #execute:   ./gitScript.sh
 
 
-read -p 'commit text: ' COMMITVAR
 git status
 git add . 
+read -p 'commit text: ' COMMITVAR
 git commit -m "${COMMITVAR}"
+git push
 
 
 
+
+#****************************************************************************************************************#
+#                                                   REFERENCIAS      	                                         #
+#https://ryanstutorials.net/bash-scripting-tutorial/bash-input.php        										 #
+#****************************************************************************************************************#
